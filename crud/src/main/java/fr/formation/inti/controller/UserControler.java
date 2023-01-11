771f4d641bac676ac13f870a00d219b5f765451f
roxaneorn <122351943+roxaneorn@users.noreply.github.com>
2023-01-11 15:01:12 +0100

    Update UserControler.java

--- crud/src/main/java/fr/formation/inti/controller/UserControler.java
+++ crud/src/main/java/fr/formation/inti/controller/UserControler.java
@@ -27,7 +27,8 @@ public class UserControler extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int i = 0;
+		response.getWriter().append("Served at: test").append(request.getContextPath());
 	}
 
 	/**
